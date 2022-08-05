075c726e0d64b50022414252678f642918301c88
vikramshaw <shawvikram15@gmail.com>
2022-08-05 21:04:12 +0530

    11. Improving Dark Mode & Refactoring App Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import './App.css';
-import About from './cmponents/About';
+// import About from './cmponents/About';
 import Navbar from './cmponents/Navbar';
-// import TextForm from './cmponents/TextForm';
+import TextForm from './cmponents/TextForm';
+import React, {useState} from 'react';
 
 function App() {
+  
+  const [mode, setMode] = useState('light')
+
+  const toggleMode= ()=> {
+    if(mode === 'dark') {
+      setMode('light');
+      document.body.style.backgroundColor= 'white';
+    }
+    else {
+      setMode('dark');
+      document.body.style.backgroundColor= '#424e58';
+    }
+  }
+
   return (
    <>
-    <Navbar title="TextUtils" about="About TextUtils"/>
+    <Navbar title="TextUtils" about="About TextUtils" mode={mode} toggleMode={toggleMode}/>
+    
     <div className="container my-3">
-      {/* <TextForm heading="Enter the text to analyze below"/> */}
-      <About/>
+      <TextForm heading="Enter the text to analyze below" mode={mode}/>
+      {/* <About/> */}
     </div>
    </>
   );
--- src/cmponents/TextForm.js
+++ src/cmponents/TextForm.js
@@ -19,9 +19,20 @@ export default function TextForm(props) {
         setText(newText)
     }
 
+    const handleCopy = () => {
+        let text = document.getElementById("mybox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
+    const handleRemoveExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
+
     const handleOnChange = (event) => {
         // console.log("On Change");
-        setText(event.target.value);
+        setText(event.target.value)
     }
 
     const [text, setText] = useState("");
@@ -30,21 +41,25 @@ export default function TextForm(props) {
     // setText("dkjbdwicow"); ----correct way to change the State
     return (
         <>
-            <div className='container'>
+            <div className='container' style={{color: props.mode === 'dark' ? 'white':'black'}}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark' ? 'grey':'white', color: props.mode === 'dark' ? 'white':'black'}} id="mybox" rows="8"></textarea>
                 </div>
                 <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
                 <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to LowerCase</button>
+                <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy</button>
+                <button className="btn btn-primary mx-2" onClick={handleRemoveExtraSpaces}>Remove Extra Spaces</button>
                 <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear</button>
             </div>
             
-            <h1>Your Text Summary</h1>
-            <p>Number of words: {text.split(" ").length} and Number of characters: {text.length}</p>
-            <p>Time to read one word is {0.008 * text.split(" ").length}</p>
-            <h2>Preview</h2>
-            <p>{text}</p>
+            <div className="container" my-3 style={{color: props.mode === 'dark' ? 'white':'black'}}>
+                <h1>Your Text Summary</h1>
+                <p>Number of words: {text.split(" ").length} and Number of characters: {text.length}</p>
+                <p>Time to read one word is {0.008 * text.split(" ").length}</p>
+                <h2>Preview</h2>
+                <p>{text.length>0 ? text : "Enter something above to preview"}</p>
+            </div>
         </>
     )
 }
